44672da97f578f87cca8190828fc3bda695f3047
Syidwan <nirvvan.rasyid@gmail.com>
2024-10-16 18:43:25 +0700

    Initial commit

---
+++ app/src/main/java/lat/pam/recyclerviewsample/MainActivity.kt
@@ -0,0 +1,42 @@
+package lat.pam.recyclerviewsample
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import lat.pam.recyclerviewsample.ItemsViewModel
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
+
+
+
+        recyclerview.layoutManager = LinearLayoutManager(this)
+
+
+        val data = ArrayList<ItemsViewModel>()
+
+
+        for (i in 1..20) {
+            data.add(ItemsViewModel("Item " + i))
+
+
+        }
+
+
+
+        val adapter = CustomAdapter(data)
+        Log.d("test string","Jumlah data: "+data.size)
+
+        recyclerview.adapter = adapter
+    }
+}
